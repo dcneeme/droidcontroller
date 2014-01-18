05684c7f573d08e3710569e94b0b22dc17a907d9
Cougar <cougar@random.ee>
2014-01-18 22:02:45 +0200

    Thread safe InData operations and copy method

--- droidcontroller/indata.py
+++ droidcontroller/indata.py
@@ -1,14 +1,19 @@
 import time
+import threading
 
 class InData():
     ''' Implementation of data buffer to store last controller readings
     '''
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         ''' Initialize buffer
         '''
 
-        self.data = {}
+        if 'data' in kwargs:
+            self.data = kwargs['data']
+        else:
+            self.data = {}
+        self.lock = threading.Lock()
 
     def write(self, key, value):
         ''' Write value to the buffer
@@ -17,9 +22,15 @@ class InData():
         :param value: The value
 
         '''
-        self.data[key] = {}
+        self.lock.acquire()
+        if key in self.data:
+            self.data[key]['old_timestamp'] = self.data[key]['timestamp']
+            self.data[key]['old_value'] = self.data[key]['value']
+        else:
+            self.data[key] = {}
         self.data[key]['timestamp'] = time.time()
         self.data[key]['value'] = value
+        self.lock.release()
 
     def read(self, key):
         ''' Read timestamp,value pair from the buffer
@@ -28,9 +39,50 @@ class InData():
         :return: [ timestamp, value ] list
 
         '''
+        return self.__read(key, '')
+
+    def read_old(self, key):
+        ''' Read previous timestamp,value pair from the buffer if exists
+
+        :param key: The register id
+        :return: [ timestamp, value ] list
+
+        '''
+        return self.__read(key, 'old_')
+
+    def __read(self, key, prefix):
+        self.lock.acquire()
         if key in self.data:
-            return {
-                    'timestamp': self.data[key]['timestamp'],
-                    'value': self.data[key]['value']}
+            if (prefix + 'timestamp') in self.data[key]:
+                res = {
+                        'timestamp': self.data[key][prefix + 'timestamp'],
+                        'value': self.data[key][prefix + 'value']}
+                self.lock.release()
+                return res
+            else:
+                self.lock.release()
+                raise Exception('previous data for key not exists')
         else:
+            self.lock.release()
             raise Exception('key not exists')
+        self.lock.release()
+
+    def copy(self):
+        return self.__copy__()
+
+    def __copy__(self):
+        self.lock.acquire()
+        newdata = self.data.copy()
+        self.lock.release()
+        return InData(data=newdata)
+
+    def __str__(self):
+        data = self.__copy__().data
+        s = ''
+        for key in data.keys():
+            s += 'key=\"' + str(key) + '\"'
+            for var in (['value', 'timestamp', 'old_value', 'old_timestamp']):
+                if var in data[key]:
+                    s += ' ' + var + '=' + str(data[key][var])
+            s += '\n'
+        return s
