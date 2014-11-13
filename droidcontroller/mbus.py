8ad7acc0096d3fc6e1e391268c0f5141e9c8029e
Neeme Takis <neeme@takis.ee>
2014-11-13 10:21:01 +0200

    works for py3, dropped py2 support for now

--- droidcontroller/mbus.py
+++ droidcontroller/mbus.py
@@ -5,19 +5,23 @@
 # m.read()
 # m.get_temperature()
 
+# ser.write() needs to be changed for py3
+# self.ser.write('\x10\x7B\xFE\x79\x16') # TypeError: an integer is required
 
+
+from codecs import encode # for encode to work in py3
 import time
 import serial
 import traceback
 import sys, logging
-#logging.basicConfig(stream=sys.stderr, level=logging.INFO)
-#logging.getLogger('acchannels').setLevel(logging.DEBUG) # yks esile
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+logging.getLogger('acchannels').setLevel(logging.DEBUG) # yks esile
 log = logging.getLogger(__name__)
 
 class Mbus:
     ''' Read various utility meters using Mbus, speed 2400, 8E1   '''
 
-    def __init__(self, port='COM27', tout=3, speed=2400, model='kamstrup602'):  # tout 1 too small!
+    def __init__(self, port='/dev/ttyUSB0', tout=3, speed=2400, model='kamstrup602'):  # tout 1 too small! win port like 'COM27'
         self.tout = tout
         self.speed = speed
         self.port = port
@@ -35,7 +39,7 @@ class Mbus:
         return self.model
         
     
-    def decode(self, invar, key=''):
+    def mb_decode(self, invar, key=''): # invar is the byte index in the read result from tty!
         ''' Returns decoded value from Mbus binary string self.mbm.
             If key (2 bytes as hex string before data start) is given, 
             then it iwill used for data verification.
@@ -43,7 +47,8 @@ class Mbus:
         try:
             res = 0
             for i in range(4):
-                res += int(ord(self.mbm[invar + i])) << (i * 8)
+                #res += int(ord(self.mbm[invar + i])) << (i * 8) # ok for py2, but not for py3
+                res += self.mbm[invar + i] << (i * 8) # py3
             if key != '' and str(key.lower()) != self.mbm[invar-2:invar].encode('hex_codec') and str(key.upper()) != self.mbm[invar-2:invar].encode('hex_codec'):
                 log.warning('possible non-matching key '+key+' in key+data '+ self.mbm[invar-2:invar].encode('hex_codec')+' '+self.mbm[invar:invar+4].encode('hex_codec'))
                 return None
@@ -57,12 +62,13 @@ class Mbus:
         
     def read(self):
         ''' Read and keep the answer from the Mbus device '''
-        #ser.open()
-        self.ser.write('\x10\x7B\xFE\x79\x16') # query to mbus device
+        #self.ser.write('\x10\x7B\xFE\x79\x16') # OK for py2.7 BU T NOT 3.x! 
+        self.ser.write(b'\x10\x7B\xFE\x79\x16') # works for both
+        
         self.mbm = self.ser.read(253) # should be 254 bytes, but the first byte E5 disappears??
         if len(self.mbm) > 0:
             log.debug('got from mbus ' + str(len(self.mbm)) + ' bytes')
-            print(self.mbm.encode('hex_codec')) # debug
+            print(encode(self.mbm, 'hex_codec')) # universal, works for 3,3 too
             return 0
         else:
             log.debug('no answer from mbus device')
@@ -78,10 +84,10 @@ class Mbus:
         for i in range(len(self.mbm)):
             if i>4:
                 if invar != '':
-                    if invar in self.mbm[i:i+4].encode('hex_codec'):
-                        print(i, self.mbm[i-2:i].encode('hex_codec'), self.mbm[i:i+4].encode('hex_codec'), self.decode(invar))
+                    if str(invar) in str(encode(self.mbm[i:i+4], 'hex_codec')):
+                        print(i, encode(self.mbm[i-2:i], 'hex_codec'), encode(self.mbm[i:i+4], 'hex_codec'))
                 else:
-                    print(i, self.mbm[i-2:i].encode('hex_codec'), self.mbm[i:i+4].encode('hex_codec'), self.decode(invar))
+                    print(i, str(encode(self.mbm[i-2:i], 'hex_codec')), str(encode(self.mbm[i:i+4], 'hex_codec')))
         
         
     def get_energy(self):
@@ -98,7 +104,7 @@ class Mbus:
             log.warning('unknown model '+self.model)
             return None
             
-        return self.decode(start, key) * coeff
+        return self.mb_decode(start, key) * coeff
             
     
     def get_volume(self):
@@ -115,7 +121,7 @@ class Mbus:
             log.warning('unknown model '+self.model)
             return None
             
-        return self.decode(start, key) * coeff
+        return self.mb_decode(start, key) * coeff
         
         
     def get_power(self):
@@ -132,14 +138,14 @@ class Mbus:
             log.warning('unknown model '+self.model)
             return None
             
-        return self.decode(start, key) * coeff
+        return self.mb_decode(start, key) * coeff
     
     
     def get_temperature(self):
         ''' Return temperature reading extracted from the last read result. Chk the datetime in self.mbm as well! '''
         key =''
         coeff = 1
-        divisor = 1
+        coeff = 1
         if self.model == 'kamstrup602':
             start = 45
             key = '0459'
@@ -150,6 +156,13 @@ class Mbus:
             log.warning('unknown model '+self.model)
             return None
             
-        return self.decode(start, key) * coeff # converted to degC
+        return self.mb_decode(start, key) * coeff # converted to degC
         
-        
+
+if __name__ == '__main__':
+    m=Mbus()
+    m.read()
+    print('result',m.mb_decode(45))
+    m.debug(45)
+    
+    
