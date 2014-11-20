f7897156b475a5558f1af007db7ce037a9006d7a
Neeme Takis <neeme@takis.ee>
2014-11-20 21:02:43 +0200

    added on_tout, set_tout(), get_tout()

--- droidcontroller/statekeeper.py
+++ droidcontroller/statekeeper.py
@@ -10,41 +10,71 @@ class StateKeeper: #
         TODO: add upfilter, time to get another up, must be smaller than off_tout
         '''
 
-    def __init__(self, off_tout = 300, in_tout = 30): # falls to down (connstate = 0) after no setting up for off_tout
+    def __init__(self, off_tout = 120, on_tout = 0): # falls to down (connstate = 0) after no setting up for off_tout
         ''' First up() while in state 0 does not change state unless in_tout == 0.
             If the second up() arrives before in_tout, state will go up.
+            off_tout value None will keep it up forever without recurring up() events.
+            off_tout value 0 makes state a pulse for exactly one get_status().
+            on_tout 0 will start upstate with first up() event.
+            on_tout >0 will with for another up() for defined tout seconds.
+            on_tout None is illegal. tout values in seconds.
         '''
-        self.off_tout = off_tout
+        self.off_tout = off_tout # down if no up() events during this.
+        self.on_tout = on_tout
         self.neverup = 1 #goes down with first up
         self.ts_up = 0
         self.ts_uplast = 0
         self.ts_dn = time.time()
         self.ts_dnlast = time.time()
+        self.upwait = 0 # intermediate state used between down to up transition
         self.state = 0 # down initially
         log.info('StateKeeper instance created')
 
+
+    def get_tout(self):
+        ''' Returns timeout values for off and on as tuple '''
+        return self.off_tout, self.on_tout
+
+
+    def set_tout(self, off_tout=None, on_tout=0):
+        ''' Sets timeout values for off and on switching. '''
+        self.off_tout = off_tout
+        self.on_tout = on_tout
+
+
     def up(self):
-        ''' Turns state up, next similar signal must arrive before timeout to keep it up '''
-        if self.state == 0:
-            self.state = 1
-            self.ts_up = time.time()
-            if self.neverup == 1:
-                log.info('conn state changed to up for the first time')
-                self.neverup = -1
-            else:
-                log.info('conn state changed to up')
-        else:
-            log.debug('conn state was already up, no change')
+        ''' Turns state up if less than on+tout s from previous up() evemnt.
+            Next similar signal must arrive before off_out to keep it up
+        '''
         self.ts_uplast = time.time()
+        if self.state == 0:
+            if self.upwait == 1 and time.time() - self.ts_uplast < self.on_tout: # sure on
+                log.debug('switching ON')
+                self.state = 1
+                self.ts_up = time.time()
+                if self.neverup == 1:
+                    log.info('state 1st ON')
+                    self.neverup = -1
+
+            elif self.upwait == 0:
+                self.upwait = 1
+                if self.on_tout == 0:
+                    log.debug('switching ON')
+                    self.state = 1
+                    self.ts_up = time.time()
+                    if self.neverup == 1:
+                        log.info('state 1st ON')
+                        self.neverup = -1
+
+
+
 
     def dn(self):
+        self.ts_dnlast = time.time()
         if self.state == 1:
             self.state = 0
             self.ts_dn = time.time()
             log.info('state changed to down')
-        else:
-            log.debug('state was already down, no change')
-        self.ts_dnlast = time.time()
 
 
     def toggle(self):
