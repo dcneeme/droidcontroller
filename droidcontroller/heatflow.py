c2afbc167d828629749b169d4b4bd1803d36253d
Neeme Takis <neeme@takis.ee>
2014-11-21 23:48:47 +0200

    added set_energy, for cumulative J restoration from server

--- droidcontroller/heatflow.py
+++ droidcontroller/heatflow.py
@@ -18,10 +18,10 @@ import logging
 log = logging.getLogger(__name__)
 
 class FlowRate:
-    """ Class to calculate values related to heat exchange metering.
+    ''' Class to calculate values related to heat exchange metering.
     Suitable in cases where flow signal (like pump state) is available
     in addition to relatively rare S0 pulses from flowmeter.
-    """
+    '''
     def __init__(self, litres_per_pulse=10):
         self.litres_per_pulse = litres_per_pulse
         self.flowrate = None
@@ -32,7 +32,7 @@ class FlowRate:
         log.info('FlowRate init')
 
     def output(self, di_pump, di_pulse):
-        """Returns flow rate l/s based on pulse count slow increment from
+        ''' Returns flow rate l/s based on pulse count slow increment from
         fluid flow meter, usually with symmetrical (50% active) pulse
         output.
 
@@ -44,7 +44,7 @@ class FlowRate:
         Averages the flow rate with the last calculated result. Result is
         likely to vary depending on the temperature of the pumped fluid.
         Execute this at DI polling speed, will skip unnecessary execs.
-        """
+        '''
         tsnow = time.time()
         flowrate = 0
         if di_pump == 1:
@@ -81,14 +81,14 @@ class FlowRate:
 
 
 class HeatExchange:
-    """Class to calculate values related to heat exchange metering.
+    '''Class to calculate values related to heat exchange metering.
     Suitable in cases where flow signal (like pump state) is available
     and the flow rate of the pump is known (can be updated).
 
     Default parameters correspond to ethylen-glycol 30%
-    
+
     for water use cp1=4200, tp1=20, cp2=4200, tp2=50
-    """
+    '''
     def __init__(self, flowrate, cp1=3776, tp1=26.7,
                                  cp2=3919, tp2=93.3, interval=10):
         self.di_pump = 0 # pump off initally
@@ -106,40 +106,42 @@ class HeatExchange:
         log.info('HeatExchange init')
 
     def set_flowrate(self, flowrate):
-        """Updates flow rate for pump based on actual flowmeter pulse
+        '''Updates flow rate for pump based on actual flowmeter pulse
         processing.
 
         Use FlowRate class to find the flowrate value based on flowmeter
         pulses.
-        """
+        '''
         self.flowrate = flowrate
 
+    def set_energy(self, invar):
+        ''' Sets cumulative energy if needed to be restored '''
+        self.energy = invar # J
+
     def get_flowrate(self):
-        """Returns flow rate for pump based on actual flowmeter pulse
-        processing
-        """
+        ''' Returns flow rate for pump based on actual flowmeter pulse processing '''
         return self.flowrate
 
     def get_specificheat(self):
-        """Returns specific heat of agent (J/(K*kg)) that depends on
+        ''' Returns specific heat of agent (J/(K*kg)) that depends on
         average agent temperature
-        """
+        '''
         return self.cp
 
     def output(self, di_pump, Ton, Tret):
-        """Returns tuple of W, J, s based on pump state, Ton, Treturn,
+        '''Returns tuple of W, J, s based on pump state, Ton, Treturn,
         flowrate, specific heat and temperature of pumped agent in 2 points.
         Result also depends on changing flowrate (update often).
         Execute this at DI polling speed, will skip unnecessary
         recalculatsions during pumping session if interval is not passed
         since last execution.
-        """
+        '''
         # average specific heat based on onflow nand return temperatures
         tsnow = time.time()
         Tdiff = Ton - Tret
         # interpolated specific energy for heat agent based on
         # average temperature
-        self.cp = self.interpolate((Ton + Tret)/2, self.tp1, self.cp1,
+        self.cp = self.interpolate((Ton + Tret)/2.0, self.tp1, self.cp1,
                                                    self.tp2, self.cp2)
         if di_pump != self.di_pump: # pump state changed
             self.di_pump = di_pump
@@ -156,9 +158,10 @@ class HeatExchange:
         else: # no chg in pump state
             if di_pump == 1: # PUMPING
                 if (tsnow > self.ts_start + self.interval or
-                        Tdiff > 1.5*self.Tdiff or
-                        Tdiff < 0.5*self.Tdiff):
-                    # do not recalculate too often during pumping
+                        Tdiff > 1.5 * self.Tdiff or
+                        Tdiff < 0.5 * self.Tdiff):
+                    # do not recalculate too often during pumping, 
+                    # there will be to many small pieces added up
                     self.power = self.flowrate * Tdiff * self.cp
                     self.energy += (tsnow - self.ts_start) * self.power
                     self.ptime += (tsnow - self.ts_start)
@@ -167,17 +170,17 @@ class HeatExchange:
             else:
                 self.power = 0
 
-        log.info('flowrate = %d, Ton = %d, Tret = %d',
+        log.debug('flowrate = %d, Ton = %d, Tret = %d',
                   self.flowrate, Ton, Tret)
-        return self.power, self.energy, self.ptime
+        return self.power, self.energy, self.ptime # W J s
 
     def interpolate(self, x, x1=0, y1=0, x2=0, y2=0):
-        """Returns linearly interpolated value y based on x and
+        ''' Returns linearly interpolated value y based on x and
         two known points defined by x1,y1 and x2,y2
-        """
+        '''
         if x1 == y2:
             log.warning('invalid interpolation attempt')
             # return average in case ponts have the same x coordinate
-            return (y1+y2)/2
+            return (y1+y2)/2.0
         else:
             return y1+(y2-y1)*(x-x1)/(x2-x1)
