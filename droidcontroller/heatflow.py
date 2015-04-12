d832be844a08e8967a5315ce3c6f1e5ac283f049
Neeme Takis <neeme@takis.ee>
2015-04-12 18:15:50 +0300

    added get_energylast() for COP calculations per HP cycle

--- droidcontroller/heatflow.py
+++ droidcontroller/heatflow.py
@@ -12,7 +12,7 @@ usage:
     fr.output(1,1)
     he.output(1,40,30)
 
-    2015 added class COP to be used with heat pumps 
+    to get COP value use self.energylast in J divided by el en  consum inc since last cycle stop!
 '''
 
 import time
@@ -30,7 +30,7 @@ class FlowRate:
         self.di_pulse = 0
         self.di_pump = 0
         self.pstate = 0
-        self.ts_start = 0
+        self.ts_last = 0
         log.info('FlowRate init')
 
     def output(self, di_pump, di_pulse):
@@ -58,15 +58,15 @@ class FlowRate:
                     # this edge active
                     if self.pstate == 0:
                         # first pulse during pumping session
-                        self.ts_start = tsnow
+                        self.ts_last = tsnow
                         self.pstate = 1
                         #self.avg = 1 # average on first pulse interval
                     else:
-                        if tsnow > self.ts_start and self.ts_start != 0:
+                        if tsnow > self.ts_last and self.ts_last != 0:
                             # update on every pulse
-                            flowrate = self.litres_per_pulse / (tsnow - self.ts_start)
+                            flowrate = self.litres_per_pulse / (tsnow - self.ts_last)
                             # count again for each pulse
-                            self.ts_start = tsnow
+                            self.ts_last = tsnow
                         else:
                             log.warning('invalid flowrate timing')
 
@@ -93,21 +93,24 @@ class HeatExchange:
     '''
     def __init__(self, flowrate, cp1=3776, tp1=26.7,
                                  cp2=3919, tp2=93.3, interval=10):
+        #flowrate unit l/s
         self.di_pump = 0 # pump off initally
         self.cp1 = cp1
         self.tp1 = tp1
         self.cp2 = cp2
         self.tp2 = tp2
         self.interval = interval # do not recalculate more often than that during pump on
-        self.ts_start = 0 # not pumped since initialization
+        self.ts_last = 0 # not pumped since initialization
         self.ts_stop = 0
-        self.Tdiff = 0 # degC
+        ##self.Tdiff = 0 # degC
         self.energy = 0 # cumulative J
-        self.ptime = 0 # cumulative s
+        self.ptime = 0 # cumulative s1
         self.set_flowrate(flowrate) # may change with temperature change
-        self.cop = None # COP of the heat pump, last cycle
+        self.energypos = 0 # restore from server if used
+        self.energyneg = 0 # restore from server if used
+        self.energycycle = 0 # sum energy during ongoing cycle
+        self.energylast = 0 # sum energy from last cycle, use for cop calc
         self.flow_threshold = None # if not None, then used or cycle syncing
-        #self.avgcop = None # average over all time and energy / non need for this!
         log.info('HeatExchange init')
 
     def set_flowrate(self, flowrate):
@@ -168,17 +171,13 @@ class HeatExchange:
         '''
         return self.cp
 
-    def output(self, di_pump, Ton, Tret):
-        ''' Older use cases without COP - do not use the available COP value! '''
-        return output2(Ton, Tret, di1 = di_pump, di2 = 1)
     
-    def output2(self, Ton, Tret, di1 = None, di2 = None):
+    def output(self, di_pump, Ton, Tret):
         '''Returns tuple of current power W, 
         cumulative energy J (updated at the next cycle start!)
         and cumulative active time s 
-        based on 
-        pump state di_pump (0 or 1, where 1 means both compressor and flow active). 
-        temperatures Ton, Treturn (temperature of pumped agent in 2 points),
+        based on pump state di_pump (0 or 1, where 1 means both compressor and flow active). 
+        temperatures Ton, Treturn (temperatures of heat exchange agent),
         flowrate and specific heat of the agent (may depend on temperature!).
         
         Result also depends on changing flowrate (update often).
@@ -188,73 +187,82 @@ class HeatExchange:
         
         To get value in Wh divide value in J by 3600.
         
-        To calculate COP of the heat pump use energy change of 
-        cumulative energy and time (not only working time). 
-        Short term COP has to be calculated for previous cycle, avoiding data from current cycle
-        (or the result would otherwise heavily depend on query timing within the cycle).
-        If however there is no new cycle and both compressor and flow are stopped, this can be informed
-        using active (1) di_off signal and then the last cycle value is calculated. This
-        value gets updated however during next cycle start, if there has been any change in cumulative energies.
-        
-        This method updates self.cop in the beginning of each new cycle start or @ di_off.
-        To get the COP value, use get_cop()!        
+        To enable COP calculation externally the energy produced during last cycle is kept up too. 
         '''
-        if self.threshold == None or (di1 == None and di2 == None):
-            log.warning('no cop calc due to no syncing infomation - threshold or di1 and di2')
-            self.cop = None
             
-        # average specific heat based on onflow nand return temperatures
+        # average specific heat based on onflow and return temperatures
         tsnow = time.time()
+        ts_diff = tsnow - self.ts_last
         Tdiff = Ton - Tret
+        ##print('timediff', ts_diff, 'tempdiff', Tdiff)
         # interpolated specific energy for heat agent based on
-        # average temperature
+        # average temperature for agent, has effect on specific heat
         self.cp = self.interpolate((Ton + Tret)/2.0, self.tp1, self.cp1,
                                                    self.tp2, self.cp2)
         #cycle start (or end if no new start coming) must be detected
         if di_pump != self.di_pump: # pump state changed
             self.di_pump = di_pump
             if self.di_pump == 1: # start
-                self.ts_start = tsnow
                 self.power = self.flowrate * Tdiff * self.cp
+                self.energycycle = 0 # new cycle started, use this value for cop calculation
+                self.ts_last = tsnow
+                ts_diff = 0 
+                log.info('heat pump cycle started')
+                
             else: # stop
-                ts_diff = tsnow - self.ts_start
-                self.energy += ts_diff * self.flowrate * self.cp
+                energydelta = ts_diff * Tdiff * self.flowrate * self.cp
+                self.energy += energydelta
+                self.energycycle += energydelta # last pumping cycle
+                self.energylast = self.energycycle
                 self.ptime += ts_diff
                 self.power = 0
+                if Tdiff > 0: # energypos increase
+                    self.energypos += energydelta
+                elif Tdiff < 0:
+                    self.energyneg -= energydelta # value positive, meaning negative
+                log.info('heat pump cycle stopped, produced energy during cycle J '+str(round(self.energylast)))
+                
 
-            self.Tdiff = Tdiff
+            ##self.Tdiff = Tdiff
         else: # no chg in pump state
             if di_pump == 1: # PUMPING
-                if (tsnow > self.ts_start + self.interval or
-                        Tdiff > 1.5 * self.Tdiff or
-                        Tdiff < 0.5 * self.Tdiff):
-                    # do not recalculate too often during pumping, 
-                    # there will be to many small pieces added up
-                    self.power = self.flowrate * Tdiff * self.cp
-                    self.energy += (tsnow - self.ts_start) * self.power
-                    self.ptime += (tsnow - self.ts_start)
-                    self.ts_start = tsnow
-                    self.Tdiff = Tdiff
+                #if (ts_diff > self.interval or
+                #        Tdiff > 1.5 * self.Tdiff or
+                #        Tdiff < 0.5 * self.Tdiff):
+                self.power = self.flowrate * Tdiff * self.cp
+                energydelta = ts_diff * self.power
+                if self.power > 0: # energypos increase
+                    self.energypos += energydelta
+                elif self.power < 0:
+                    self.energyneg -= energydelta # value positive, meaning negative
+                self.energy += energydelta
+                self.energycycle += energydelta
+                self.ptime += (tsnow - self.ts_last)
+                self.ts_last = tsnow
+                ##self.Tdiff = Tdiff
             else:
                 self.power = 0
 
         log.debug('flowrate = %d, Ton = %d, Tret = %d',
                   self.flowrate, Ton, Tret)
-        return self.power, self.energy, self.ptime # W J s
+        return self.power, self.energy, self.ptime, self.energypos, self.energyneg # W J s J J 
+        # last two are for separating the produced heat (energypos) from the production loss for heat pump melting (energyneg)
 
 
-    def get_cop(self):
-        ''' Returns COP based on last heat pump cycle, from start to start or from start to cop_timeout.
-        Should return None if the last cycle has ended for more than cop_timeout s ago. 
-        The timing for calculation is set by output(), thus output must be called frequently. '''
-        
-        return self.cop
+    def get_energylast(self):
+        ''' Returns summary energy from last cycle generatred by output(), use for COP calc '''
+        return self.energylast # J
 
     
-    def set_cop(self, cop = None):
-        ''' Sets former COP values, last and avg '''
-        if cop != None:
-            self.cop = cop # no need for average over cumulative, see charts#
+    def set_energypos(self, invar):
+        ''' Restores produced positive heat '''
+        if invar != None:
+            self.energypos = invar # produced useful heat, excluding melting energy
+                    
+    def set_energyneg(self, invar):
+        ''' Restores produced melting heat energy '''
+        if invar != None:
+            self.energyneg = invar # produced useful heat, excluding melting energy
                     
     
     def interpolate(self, x, x1=0, y1=0, x2=0, y2=0):
@@ -269,8 +277,4 @@ class HeatExchange:
             return y1+(y2-y1)*(x-x1)/(x2-x1)
 
 
-#class COP(HeatEcxhange):
-#    ''' Returns heat pump COP based on time and energy. HeatExchange variables usable? '''
-#    def __init__(self, active, passive = 0):
-        
             
