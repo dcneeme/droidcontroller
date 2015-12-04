bf463401b7fa7cf2b89130ef8f101b84137cb8f2
Neeme Takis <neeme@takis.ee>
2015-12-04 02:16:19 +0200

    missing self

--- droidcontroller/heating.py
+++ droidcontroller/heating.py
@@ -76,7 +76,8 @@ class Heater(object): # Junkers Euromaxx for now.
                 
             self.read_svcs() # refresh TGW, THW, KPPW, KGIW, KGDW values
             
-            self.pwm_values = [ UN.val2int(self.pid_gas[0].output(self.TGW[2],self.TGW[0],noint=noint)), UN.val2int(self.pid_gas[1].output(THW[2], self.THW[0], noint=noint)) ]
+            self.pwm_values = [ UN.val2int(self.pid_gas[0].output(self.TGW[2], self.TGW[0], noint=noint)), 
+                                UN.val2int(self.pid_gas[1].output(self.THW[2], self.THW[0], noint=noint)) ]
             
             self.write_svcs()  # outputs setting, incl pwm
             
