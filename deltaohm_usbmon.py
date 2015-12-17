544b90b1f920f8f9d2f088c8aa4224ae4751e07f
Neeme Takis <neeme@takis.ee>
2015-12-17 23:10:06 +0200

    seems ready, able to recover from usb or modbus failure

--- deltaohm_usbmon.py
+++ deltaohm_usbmon.py
@@ -1,7 +1,11 @@
 # use serial port COM11 for delta instrument
 # send 'HA\r\n'
 # read Date=2014/09/22 14:44:52;   622;     0;   49.8;   23.3;   999;  ERR. ;  ERR. ;  ERR. ;   12.3;   10.4;    8.9;   16.5;   46.0
-
+''' to be used on pc with deltaohm meter connected via usb.
+   sensors to be calibrate should be accessed via modbus.
+   there is one service per metered value as reference 
+   and others with reference value as the first member.
+'''
 from codecs import encode # for encode to work in py3
 import time
 import serial
@@ -15,23 +19,30 @@ import serial.tools.list_ports
 #print(list(serial.tools.list_ports.comports()))
 
 from droidcontroller.uniscada import UDPchannel
+from droidcontroller.comm_modbus import CommModbus
 
 class DeltaOhm(object):
     ''' Using DeltaOhm HD21AB17 as monitoring info source '''
     def __init__(self, port='auto', autokey='TUSB3410', tout=1,
             model='HD21AB17', conf={
-                'DMC2V': [1, 1, 'co2 ppm'],
+                'DMC2W': [1, 1, 'co2 ppm', {0: [11, 1]}],
                 'DMC1V': [2, 1, 'co ppm'],
                 'DMHV': [3, 10, 'hum %'],
-                'DMTV': [4, 10, 'temp degC'],
+                'DMTW': [4, 10, 'temp degC', {0: [11, 2]}],
                 'DMPV': [5, 1, 'pressure hPa']
             },
             speed=460800, parity='N',
             interval = 60,
             id = '010000000010',
-            ip='195.222.15.51'
+            ip='195.222.15.51',
+            mbiconf={0:'10.0.0.4:10001'}
             ):
 
+        ''' in configuration one reference and several tested values can be described
+            (colum, coeff, name] for ref and [mbi, mba, regadd] for tested value(s).
+            channels to be tested are defined with mbiconf {mbi:'10.0.0.4:10001', mbi:'COM30|19200|E'} 
+            use None for mbiconf if there are no channels to test.
+            '''
         self.interval = interval # between readings
         self.id = id # monitooringus host
         ports = list(serial.tools.list_ports.comports())
@@ -66,13 +77,29 @@ class DeltaOhm(object):
         self.conf = conf
         
         self.udp = UDPchannel(id=id, ip=ip)
-
+        
+        if mbiconf:
+            self.mb = []
+            self.host = []
+            self.port = []
+            for mbi in range(len(mbiconf)):
+                if ':' in mbiconf[mbi]:
+                    self.host.append(mbiconf[mbi].split(':')[0])
+                    self.port.append(int(mbiconf[mbi].split(':')[1]))
+                    self.mb.append(CommModbus(host = self.host[mbi], port = self.port[mbi]))
+                #add serial here elif
+                else:
+                    log.warning('!! unsupported configuration block '+str(mbiconf[mbi]))
+                    
+                
+        log.info('### setup done ####')
+        
     
     def read(self):
         ''' return all available (defined in self.conf) values in a list format '''
         try:
             self.ser.write('HA\r'.encode('ascii')) # CR needed
-            sendstring = 'id:'+self.id +'\n' # no in: here
+            sss = 'id:'+self.id +'\n' # no in: here # voib ka ara jaada, siis lisab udpsend!
             time.sleep(0.5)
             res = self.ser.readline().decode('ascii')
             if len(res) < 100: # powered down?
@@ -80,23 +107,51 @@ class DeltaOhm(object):
                 print('no deltaohm response')
                 return None
             print(res)
-
-            cols = res.split(';')
-            for svc in self.conf:
-                #print(svc)
-                #print(self.conf[svc])
-                #print(eval(str(cols[self.conf[svc][0]])))
-                #print(eval(str(self.conf[svc][1])))
-                numvalue = int(eval(str(cols[self.conf[svc][0]])) * eval(str(self.conf[svc][1])))
-                sys.stdout.write(self.conf[svc][2]+' '+str(cols[self.conf[svc][0]])+' -> '+str(numvalue)+',   ') # to see retries
-                sendstring +=  svc+':'+str(numvalue)+'\n'+svc[:-1]+'S:0\n'
-            sys.stdout.write('\n')
-            sys.stdout.flush()
-            return sendstring
         except:
             log.error('FAILURE to communicate with deltaohm')
             traceback.print_exc()
             return None
+            
+        # usb info olemas, nyyd lisaandurid
+        
+        cols = res.split(';')
+        for svc in self.conf:
+            print('svc',svc)
+            #print(self.conf[svc])
+            #print(eval(str(cols[self.conf[svc][0]])))
+            #print(eval(str(self.conf[svc][1])))
+            numvalue = int(eval(str(cols[self.conf[svc][0]])) * eval(str(self.conf[svc][1])))
+            #sys.stdout.write(self.conf[svc][2]+' '+str(cols[self.conf[svc][0]])+' -> '+str(numvalue)+',   ') # to see retries
+            sendstring =  svc+':'+str(numvalue) # starting the string for this service
+            
+            if len(self.conf[svc]) > 3: # channels to be tested present
+                # 'DMC2W': [1, 1, 'co2 ppm', {0: [11, 1]}],
+                dict2test = self.conf[svc][3] # all channels in one dict
+                for mbi in dict2test: 
+                    mba = dict2test[mbi][0]
+                    reg = dict2test[mbi][1]
+                    print('mbi,mba,reg',mbi,mba,reg) ##
+                    i = 0
+                    res = None
+                    while res == None and i < 2:
+                        try:
+                            res = self.mb[mbi].read(mba, reg, 1)[0]
+                        except:
+                            log.warning('mbus comm FAILED, trying to recreate channels mbi '+str(mbi))
+                            self.mb[mbi] = CommModbus(host = self.host[mbi], port = self.port[mbi])
+                            time.sleep(0.5) # proovime kas asi paraneb
+                            res = None
+                    if res != None:
+                        sendstring += ' '+str(res)
+                    else:
+                        sendstring = ''
+                if sendstring != '':
+                    sendstring += '\n'+svc[:-1]+'S:0\n' # status always 0 for now
+            else: # one (reference) member only
+                sendstring += '\n'+svc[:-1]+'S:0\n'
+            sss += sendstring                
+        return sss
+        
         
     
     def comm(self, sendstring):
@@ -119,5 +174,5 @@ if __name__ == '__main__':
                 d.ser.open()
             except:
                 log.error('FAILED to close or open serial port!')
-        time.sleep(d.interval)
-        #time.sleep(5) # test
+        time.sleep(d.interval) # 60 s puhul ei pysi wifi232 yhendus!
+        #time.sleep(10) # test
