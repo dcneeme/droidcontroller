e669704b4ff860c79ac8d7132a8a1b3236552aaf
Neeme Takis <neeme@takis.ee>
2015-12-28 21:45:03 +0200

    sqlread and dump implemented

--- droidcontroller/gcal.py
+++ droidcontroller/gcal.py
@@ -1,6 +1,4 @@
-import os, traceback, sqlite3, time, requests
-from droidcontroller.sqlgeneral import * # SQLgeneral
-s = SQLgeneral()
+import os, traceback, sqlite3, time, subprocess, requests
 
 import logging
 log = logging.getLogger(__name__)
@@ -10,76 +8,144 @@ log = logging.getLogger(__name__)
     from droidcontroller.gcal.py import *
     cal=Gcal('00101200006')
     cal.sync()
-    cal.check('S')
-      the last one returns value for S (1 if just 'S' in event summary, 22 if 'S=22' in summary)
+    cal.check('S') # returns value for S (1 if just 'S' in event summary, 22 if 'S=22' in summary)
+
+      FIXME - does not dump table to file, to keep recurring events without connectivity...
+      FIXME - cooling/warming delay should be taken into account in heating control, to shift setpoint
 '''
 
-class Gcal:
+class Gcal(object):
     ''' Class containing methods to read events from monitoring server handing access to google calendar '''
 
-    def __init__(self, host_id, days=3): # add sync_interval?
+    def __init__(self, host_id, days=3, table='calendar', msgbus=0, out_svc=None): # able to publish to msgbus
         self.host_id = host_id
         self.days = days
-        s.sqlread('calendar')
-        self.cur = conn.cursor()
+        self.conn = sqlite3.connect(':memory:')
+        self.table = table
+        if self.sqlread(self.table) == 0: # dump read ok
+            log.info('reusing existing calendar file')
+        else: # create new table
+            Cmd = 'drop table if exists '+self.table
+            self.conn.execute(Cmd) # drop the table if it exists
+            Cmd = "CREATE TABLE calendar(title,timestamp,value); \
+                CREATE INDEX ts_calendar on 'calendar'(timestamp);"
+            self.conn.execute(Cmd)
+            self.conn.commit()
+            log.info('created new calendar table')
+
+
+    def sqlread(self, table): # drops table and reads from file <table>.sql that must exist
+        ''' restore buffer from dump. basically the same as in sqlgeneral.py '''
+        sql = ''
+        filename=table+'.sql' # the file to read from
+        try:
+            if os.path.getsize(filename) > 50:
+                msg = 'found '+filename
+                sql = open(filename).read()
+            else:
+                msg = filename+' corrupt or empty!'
+                log.info(msg)
+                time.sleep(1)
+                return 1
+        except:
+            msg = filename+' missing!'
+            log.warning(msg)
+            time.sleep(1)
+            return 1
+
+        Cmd = 'drop table if exists '+table
+        try:
+            self.conn.execute(Cmd) # drop the table if it exists
+            self.conn.commit()
+            self.conn.executescript(sql) # read the existing table into database
+            self.conn.commit()
+            msg = 'successfully recreated table '+table
+            log.info(msg)
+            return 0
+
+        except:
+            msg = filename+' corrupt: '+str(sys.exc_info()[1])
+            log.warning(msg)
+            traceback.print_exc()
+            time.sleep(1)
+            return 1
 
     def sync(self, cal_id=None): # query to SUPPORTHOST, returning all events. cal_id may be different from host_id (several cals)...
-        ''' Updates the local event buffer for days ahead. Should happen in background according to some interval... '''
+        ''' Updates the local event buffer for days ahead. Dumps the result into sql file.
+            Should happen in background according to some interval... ''' # FIXME make it async
         # example:   http://www.itvilla.ee/cgi-bin/gcal.cgi?mac=000101000001&days=10
         if not cal_id:
             cal_id = self.host_id
         req = 'http://www.itvilla.ee/cgi-bin/gcal.cgi?mac='+cal_id+'&days='+str(self.days)+'&format=json'
         headers = {'Authorization': 'Basic YmFyaXg6Y29udHJvbGxlcg=='} # Base64$="YmFyaXg6Y29udHJvbGxlcg==" ' barix:controller
         msg = 'starting gcal query '+req
-        print(msg) # debug
+        log.debug(msg) # debug
         try:
             response = requests.get(req, headers = headers)
         except:
             msg = 'gcal query '+req+' failed!'
             log.warning(msg)
             traceback.print_exc()
-            print(msg)
             return 1 # kui ei saa gcal yhendust, siis lopetab ja vana ei havita!
 
         try:
-            print('response.content', response.content)
             if '[]' in str(response.content):
                 log.warning('invalid content for calendar, keeping the existing calendar table')
                 s.print_table('calendar')
                 return 2
             else:
+                log.info('response.content', response.content)
                 events = eval(response.content) # string to list
-            
+
         except:
             msg = 'getting calendar events failed for host_id '+self.host_id
-            print(msg)
-            #log.warning(msg)
+            log.warning(msg)
             traceback.print_exc() # debug
             return 1 # kui ei saa normaalseid syndmusi, siis ka lopetab
 
         #print(repr(events)) # debug
         Cmd = "BEGIN IMMEDIATE TRANSACTION"
         try:
-            conn.execute(Cmd)
-            Cmd = "delete from calendar"
-            conn.execute(Cmd)
+            self.conn.execute(Cmd)
+            Cmd = "delete from "+self.table
+            self.conn.execute(Cmd)
             for event in events:
                 #print('event',event) # debug
                 columns = str(list(event.keys())).replace('[','(').replace(']',')')
                 values = str(list(event.values())).replace('[','(').replace(']',')')
                 Cmd = "insert into calendar"+columns+" values"+values
-                #print(Cmd) # debug
-                conn.execute(Cmd)
-            conn.commit()
-            msg = 'calendar table updated'
-            print(msg)
-            #log.warning(msg)
+                log.debug(Cmd) # debug
+                self.conn.execute(Cmd)
+            self.conn.commit()
+            self.dump() # to file
+            msg = 'calendar table updated and dumped'
+            log.info(msg)
             return 0
         except:
             msg = 'delete + insert to calendar table failed!'
             log.warning(msg)
             traceback.print_exc() # debug
             return 1 # kui insert ei onnestu, siis ka delete ei toimu
+
+
+    def dump(self): # sql table to file
+        ''' Writes the table into a SQL-file to keep the existing scheduling data '''
+        msg=self.table+' dump into '+self.table+'.sql'
+        log.info(msg)
+        try:
+            with open(self.table+'.sql', 'w') as f:
+                for line in self.conn.iterdump(): # see dumbib koik kokku!
+                    if self.table in line: # needed for one table only! without that dumps all!
+                        f.write('%s\n' % line)
+            subprocess.call('sync')
+            time.sleep(0.1)
+            return 0
+        except:
+            msg = 'FAILURE dumping '+self.table+'!'
+            log.warning(msg)
+            traceback.print_exc()
+            time.sleep(1)
+            return 1
 
 
     def check(self, title): # set a new setpoint if found in table calendar (sharing database connection with setup)
@@ -91,15 +157,18 @@ class Gcal:
 
         Cmd = "BEGIN IMMEDIATE TRANSACTION"
         try:
-            conn.execute(Cmd)
+            self.conn.execute(Cmd)
             Cmd = "select value,timestamp from calendar where title='"+title+"' and timestamp+0<"+str(tsnow)+" order by timestamp asc" # find the last passed event value
             self.cur.execute(Cmd)
             for row in self.cur:
                 value = row[0] # overwrite with the last value before now
                 ts = row[1]
                 log.debug('cal tsnow '+str(tsnow)+', ts '+str(ts)+', value '+str(value)) # debug.  viimane value jaab iga title jaoks kehtima
-            conn.commit()
+            self.conn.commit()
+            #if self.msgbus:
+            #    self.msgbus.publish(val_reg, {'values': values, 'status': sumstatus})
             return str(value) # last one for given title becomes effective. can be empty string too, then use default value for setpoint related to title
+
         except:
             traceback.print_exc()
             return None
