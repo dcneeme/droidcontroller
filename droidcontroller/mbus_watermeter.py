e8f55d3acc79191229ed4f74873bdf758f5a30b9
Neeme Takis <neeme@takis.ee>
2015-12-31 15:48:13 +0200

    read_sync() almost done

--- droidcontroller/mbus_watermeter.py
+++ droidcontroller/mbus_watermeter.py
@@ -1,36 +1,86 @@
 # neeme 2015
-import traceback, time, tornado
-from droidcontroller.mbus import *
+# marko libmbus, python-mbus
+import sys, traceback, time # , tornado
+#from droidcontroller.mbus import * # neeme old
+from mbus.MBus import MBus # by marko
+import xmltodict
+
+import tornado.ioloop
+
+from functools import partial
+from concurrent.futures import ThreadPoolExecutor
+EXECUTOR = ThreadPoolExecutor(max_workers=1)
 
 import logging
-#logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
-#logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 log = logging.getLogger(__name__)
 
 class MbusWaterMeter(object): # FIXME averaging missing!
-    ''' to be used with iomain with tornado IOloop. writes values into services. '''
-    def __init__(self, msgbus, model, svc_cum, svc_avg, avg_win = 3600):
-        #self.ac = ac # aicochannels
+    ''' Publish values to services via msgbus '''  # FIXME use IOloop too
+    def __init__(self, msgbus, svclist=[['XYW',1,1,'undefined']]): # svc, member, id, name
         self.msgbus = msgbus # all communication via msgbus, not to ac  directly!
-        
-        self.svc_cum = svc_cum # where to write reading from meter
-        self.svc_avg = svc_avg # where to write average consumption in time window
-        self.avg_win = avg_win # sliding time window length s 
+        self.svclist = svclist # svc, member, id, name
+
         try:
-            self.mbus = Mbus(model='cyble_v2') # vaikimisi port auto, autokey FTDI  # port='/dev/ttyUSB0') #
-            log.info('mbus instance created, output to msgbus: '+svc_cum)
+            self.mbus = MBus(device="/dev/ttyUSB0") ## 
+            self.mbus.connect() ## 
+            log.info('mbus instance created, output to msgbus: '+str(svclist))
         except:
-            log.warning('Mbus connection NOT possible, probably no suitable USB port found!')
+            log.error('Mbus connection NOT possible, probably no suitable USB port found!')
             traceback.print_exc()
+            time.sleep(3)
         
-    def read(self):
-        ''' Reads and returns cumulative and average sliding window values in meter units '''
+    def read_sync(self, debug = False):
+        ''' Query mbus device, waits for reply, lists all if debug == True '''
         try:
-            self.mbus.read()
-            volume = int(self.mbus.get_volume())
-            log.info('got value from water meter: '+str(volume)+', going to save into svc '+self.svc_cum)
-            self.msgbus.publish(self.svc_cum, {'values': [ volume ], 'status': 0}) # msgbus.publish(val_reg, {'values': values, 'status': status})
-            #res = self.ac.set_aivalue(self.svc_cum, 1, volume) # moved to iomain or whatever listener (based on sqlgeneral?)
+            self.mbus.send_request_frame(254)
+            reply = self.mbus.recv_frame()
+            reply_data = self.mbus.frame_data_parse(reply)
+            xml_buff = self.mbus.frame_data_xml(reply_data)
+            #print(xml_buff)
         except:
-            log.warning('watermeter read FAILED')
-            traceback.print_exc()
+            log.error('FAILED to get data from mbus')
+            return 1
+                
+        try:
+            d = xmltodict.parse(xml_buff)
+        except Exception as ex:
+            print("parse error: %s" % ex)
+            sys.exit()
+        #print(repr(d)) #
+        res = self.parse(d, debug)
+        return res        
+            
+    def parse(self, dict, debug = False):
+        found = 0
+        for x in dict['MBusData']['DataRecord']:
+            if debug == True:
+                print(x)
+            for svc in self.svclist:
+                if int(x['@id']) == svc[2]:
+                    vs = x['Value'] # , x['Unit']) ## key 'Unit' not found?  
+                    log.info('found value with id '+str(svc[2])+': '+vs ) # str
+                    self.msgbus.publish(svc[0], {'values': [ int(vs) ], 'status': 0}) # msgbus.publish(val_reg, {'values': values, 'status': status})
+                    found += 1
+        if found > 0:
+            return 0
+        else:
+            return 1
+
+
+    # methods needed for async comm        
+    def run(self):
+        self.read_async(self.async_reply)
+
+    def read_async(self, reply_cb):
+        log.info("    mbus_send_request_frame..")
+        EXECUTOR.submit(self.read_sync).add_done_callback(lambda future: tornado.ioloop.IOLoop.instance().add_callback(partial(self.callback, future)))
+
+    def callback(self, future):
+        result = future.result()
+        self.async_reply(result)
+
+    def async_reply(self, result):
+        print("    mbus result: " + str(result))
+        self.run()
+
+        
