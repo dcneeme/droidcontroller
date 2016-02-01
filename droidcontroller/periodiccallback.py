af9ab736eaa756b04501d54082255576b2bc9c3b
Neeme Takis <neeme@takis.ee>
2016-02-01 23:40:24 +0200

    marko added run_now() method to PeriodicCallback

---
+++ droidcontroller/periodiccallback.py
@@ -0,0 +1,13 @@
+import tornado.ioloop
+
+class PeriodicCallback(tornado.ioloop.PeriodicCallback):
+    ''' Use this to add a useful method to run/start callback immediately into PeriodicCallback class '''
+    def run_now(self):
+        self.stop()
+        #self._running = True
+        try:
+            return self.callback()
+        except Exception:
+            self.io_loop.handle_callback_exception(self.callback)
+        finally:
+            self.start()
