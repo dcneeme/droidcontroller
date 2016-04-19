00ccc2150f3c26e3f657adc5af8e03950f4d67c2
Neeme Takis <neeme@takis.ee>
2016-04-19 21:56:43 +0300

    added self.id into init, less talk with debug=False

--- droidcontroller/mbus_watermeter.py
+++ droidcontroller/mbus_watermeter.py
@@ -1,6 +1,6 @@
-# neeme 2015
+# neeme 2015..2016
 # marko libmbus, python-mbus
-import sys, traceback, time # , tornado
+import sys, traceback, time # tornado
 #from droidcontroller.mbus import * # neeme old
 from mbus.MBus import MBus # by marko
 import xmltodict
@@ -16,16 +16,18 @@ log = logging.getLogger(__name__)
 
 ''' Usage example
 from mbus_watermeter import MbusWaterMeter # libmbus c-library is needed too!
-m=MbusWaterMeter() # meter instance
+m=MbusWaterMeter(id=1) # meter instance, id=1 for HRI, 4 for itron
 m.read_sync() # sync read, async is also possible using future. 
 m.parse(debug=True) # to list all possible values from the meter
-m.parse(id=1) # to get the needed value only
+m.parse() # to get the needed value only
 '''
 
 class MbusWaterMeter(object): # FIXME averaging missing!
     ''' Publish values to services via msgbus '''  # FIXME use IOloop too
-    def __init__(self, msgbus=None, svclist=[['XYW',1,1,'undefined']]): # svc, member, id, name
-        self.msgbus = msgbus # all communication via msgbus, not to ac  directly!
+    def __init__(self, id = 4, msgbus=None, svclist=[['XYW',1,1,'undefined']]): # svc, member, id, name
+        ''' id=4 for itron, id=1 for HRI-B '''
+        self.id = id
+        self.msgbus = msgbus # publish to msgbus, if not None
         self.svclist = svclist # svc, member, id, name
         self.xml = ''
         self.dict = {}
@@ -40,13 +42,15 @@ class MbusWaterMeter(object): # FIXME averaging missing!
 
     def read_sync(self, debug = False):
         ''' Query mbus device, waits for reply, lists all if debug == True '''
-        log.info('sending out a sync mbus query')
+        if debug:
+            log.info('sending out a sync mbus query')
         try:
             self.mbus.send_request_frame(254)
             reply = self.mbus.recv_frame()
             reply_data = self.mbus.frame_data_parse(reply)
             self.xml = self.mbus.frame_data_xml(reply_data)
-            print(self.xml) ##
+            if debug:
+                print(self.xml)
         except:
             log.error('FAILED to get data from mbus')
             return 1
@@ -57,24 +61,24 @@ class MbusWaterMeter(object): # FIXME averaging missing!
             print("parse error: %s" % ex)
             sys.exit()
         
-    def parse(self, id=4, debug = False):
+    def parse(self, debug = False):
         ''' debug will list all values with any id. 4 is id for itron, use id=1 for HRI-B '''
         found = False
         for x in self.dict['MBusData']['DataRecord']:
             if debug == True:
                 print(x)
             for svc in self.svclist:
-                if int(x['@id']) == id: # svc[2]:
-                    vs = x['Value'] # , x['Unit']) ## key 'Unit' not found?
+                if int(x['@id']) == self.id: 
+                    vs = x['Value'] 
                     try:
                         val= int(vs)
-                        #log.info('got value with id '+str(svc[2])+': '+val ) # str
-                        log.info('got value with id 4: '+ str(val) ) # str
+                        if debug:
+                            log.info('got value with id '+str(self.id)+': '+ str(val) ) # str
                         if self.msgbus:
                             self.msgbus.publish(svc[0], {'values': [ val ], 'status': 0}) # msgbus.publish(val_reg, {'values': values, 'status': status})
                         found = True
                     except:
-                        log.error('invalid Value data from id 4: '+vs)
+                        log.error('invalid Value data from id '+str(self.id)+': '+vs)
                         traceback.print_exc()
                         
         if found:
@@ -98,5 +102,5 @@ class MbusWaterMeter(object): # FIXME averaging missing!
 
     def async_reply(self, result):
         print("    mbus result: " + str(result))
-        self.parse(result)
+        self.parse()
 
