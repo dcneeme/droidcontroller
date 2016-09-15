97f80894e74aecc668abaa8442ce9b16a1775e3e
Neeme Takis <neeme@takis.ee>
2016-09-15 18:08:45 +0300

    get_all() improvement

--- droidcontroller/mbus_meter.py
+++ droidcontroller/mbus_meter.py
@@ -17,7 +17,7 @@ try:
     import tornado.ioloop
 except:
     log.warning('no tornado, only sync mbus comm!')
-    
+
 from functools import partial
 from concurrent.futures import ThreadPoolExecutor
 EXECUTOR = ThreadPoolExecutor(max_workers=1)
@@ -35,11 +35,11 @@ m.get_flow()
 
 '''
 
-class MbusMeter(object): 
+class MbusMeter(object):
     ''' Publish values to services via msgbus '''  # FIXME use IOloop too
     def __init__(self, msgbus=None, port='/dev/ttyUSB0', model='kamstrup402', svclist=[['XYW',1,1,'undefined']], primary=254, secondary=''): # svc2publish, member, id, name
         self.primary = primary # 254 is broadcast address, usable for single meter on the line
-        self.secondary = secondary # hex string, primary addresses can be the same, if secondary addresses 
+        self.secondary = secondary # hex string, primary addresses can be the same, if secondary addresses
         self.msgbus = msgbus # all communication via msgbus, not to ac  directly!
         self.svclist = svclist # svc, member, id, name
         self.dict = {} # used by read()
@@ -56,22 +56,22 @@ class MbusMeter(object):
             traceback.print_exc()
             time.sleep(3)
         self.modeldata = {} # define only  what's important, based on xml
-        self.modeldata.update({'kamstrup402': 
-            {1:['Energy (kWh)',1,'kWh'], 2:['Volume (1e-2  m^3)',0.01,'m3'], 4:['Flow temperature (1e-2 deg C)',0.1,'ddegC'], 
+        self.modeldata.update({'kamstrup402':
+            {1:['Energy (kWh)',1,'kWh'], 2:['Volume (1e-2  m^3)',0.01,'m3'], 4:['Flow temperature (1e-2 deg C)',0.1,'ddegC'],
             5:['Return temperature (1e-2 deg C)',0.1,'ddegC'], 6:['Temperature Difference (1e-2 deg C)',0.1,'ddegC'], 7:['Power (100 W)',100,'W'], 9:['Volume flow (m m^3/h)',1,'l/h']}})
-        self.modeldata.update({'kamstrup602': 
-            {1:['Energy (kWh)',1,'kWh'], 2:['Volume (1e-2  m^3)',0.01,'m3'], 4:['Flow temperature (1e-2 deg C)',0.1,'ddegC'], 
+        self.modeldata.update({'kamstrup602':
+            {1:['Energy (kWh)',1,'kWh'], 2:['Volume (1e-2  m^3)',0.01,'m3'], 4:['Flow temperature (1e-2 deg C)',0.1,'ddegC'],
             5:['Return temperature (1e-2 deg C)',0.1,'ddegC'], 7:['Power (100 W)',100,'W'], 9:['Volume flow (m m^3/h)',1,'l/h']}})
-        self.modeldata.update({'axisSKU03': 
-            {5:['Energy (kWh)',1,'kWh'], 6:['Volume (l)',0.001,'m3'], 9:['Flow temperature (deg C)',10,'ddegC'], 
+        self.modeldata.update({'axisSKU03':
+            {5:['Energy (kWh)',1,'kWh'], 6:['Volume (l)',0.001,'m3'], 9:['Flow temperature (deg C)',10,'ddegC'],
             10:['Return temperature (deg C)',10,'ddegC'], 7:['Power (W)',1000,'W'], 8:['Volume flow (l/h)',1000,'l/h']}})
         self.modeldata.update({'cyble': {4:['Volume (l)',1,'l']}}) # water meter koogu20
         self.modeldata.update({'APA': {2:['Volume (l)',1,'l'], 3:['Volume flow (l/h))',1,'l']}}) # water meter vaatsakool
-         
-         
+
+
     def set_model(self, invar):
         self.model = invar
-        
+
     def read_sync(self, debug = False):
         ''' Query mbus device, waits for reply, lists all if debug == True '''
         log.info('sending out a sync mbus query')
@@ -79,14 +79,14 @@ class MbusMeter(object):
             sec = True
         else:
             sec = False
-        
+
         if debug:
             if sec:
                 logadd = 'secondary address '+self.secondary
             else:
                 logadd = 'primary address '+str(self.primary)
             log.info('sending out a sync mbus query using '+logadd)
-            
+
         try:
             if sec:
                 self.mbus.select_secondary_address(self.secondary)
@@ -104,7 +104,7 @@ class MbusMeter(object):
             log.error('FAILED to get data from mbus')
             traceback.print_exc()
             return 1
-       
+
         try:
             ##d = xmltodict.parse(self.xml)
             self.dom = parseString(self.xml)
@@ -115,23 +115,23 @@ class MbusMeter(object):
 
     def get_xml(self): # use read_sync(); get_xml() for debugging
         return self.xml
-        
-        
+
+
     def get_errors(self):
         return self.errors
-    
+
     def get_models():
         '''return all supported models '''
         return self.modeldata # dict
-    
+
     def get_model():
         '''return currently selected model '''
         return self.model # dict
-    
+
     def parse(self, dict, debug = False):
         ''' Publish all  '''
         return 1 # FIXME
-        
+
         found = 0
         for x in dict['MBusData']['DataRecord']:
             if debug == True:
@@ -148,7 +148,7 @@ class MbusMeter(object):
         else:
             return 1
 
-    
+
     # methods needed for async comm
     def run(self):
         self.read_async(self.async_reply)
@@ -180,7 +180,7 @@ class MbusMeter(object):
         except:
             traceback.print_exc()
             return 2
-    
+
     def parse1(self, id):
         ''' Return one Value with matching id from self.nodes '''
         try:
@@ -188,7 +188,7 @@ class MbusMeter(object):
         except:
             value = self.nodes[id].firstChild.nodeValue
         return value
-        
+
     def find_id(self, name): # name is 'Energy', 'Volume flow' or smthg...
         found = 0
         idlist = list(self.modeldata[self.model].keys())
@@ -196,12 +196,12 @@ class MbusMeter(object):
             if name in self.modeldata[self.model][id][0]:
                 found = 1
                 break
-            
+
         if found == 1:
             return id
         else:
             return None
-        
+
     def get_energy(self):
         if self.xml != '':
             id = self.find_id('Energy')
@@ -247,32 +247,32 @@ class MbusMeter(object):
         tdif = None
         if self.xml != '':
             id = self.find_id('Flow temperature')
-            ton = self.parse1(id) # 
+            ton = self.parse1(id) #
             id = self.find_id('Return temperature')
             tret = self.parse1(id) #
             if self.model == 'kamstrup402':
                 id = self.find_id('Temperature Difference')
                 tdif = self.parse1(id) #
-            
             return ton, tret, tdif
         else:
             log.error('use read() first')
         return None
-        
-        
+
+
     def get_all(self): # kogu info nagemiseks vt self.xml
         out = {}
-        name = ''
-        member = None
         conf = self.modeldata[self.model]
-        for id in conf:
-            value = self.parse1(id)
-            for svc in self.svclist:
-                if svc[2] == id:
-                    name, member = svc[0:2]
-                    
-            out.update({id:[conf[id][0], value, name, member]})
-            
-        return out
-    
-    
+        if self.xml != '':
+            for id in conf:
+                name = ''
+                member = None
+                value = self.parse1(id)
+                for svc in self.svclist:
+                    if svc[2] == id:
+                        #print('matching svc and id',svc,id)
+                        name, member = svc[0:2]
+                out.update({id:[conf[id][0], value, name, member]})
+            return list(out.values())
+        else:
+            log.error('use read() first')
+        return None
