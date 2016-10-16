d73f902c05e762c736d43b33025c35977507ef00
Neeme Takis <neeme@takis.ee>
2016-10-16 19:07:23 +0300

    fixed in readval type handlin

--- droidcontroller/ai2pwm.py
+++ droidcontroller/ai2pwm.py
@@ -13,7 +13,7 @@ log = logging.getLogger(__name__)
 class AI2pwm(object):
     ''' Takes an ai value, compares to setpoint and generates do bit value. Based on services in sql channel tables. '''
 
-    def __init__(self, ac, name='none', set=['C02W',2], act=(['C02W',1]), out=['V1W',1], min=20, max=95, inv=False, bias=0xc000, deadband=1): # svc - name, member. 
+    def __init__(self, ac, name='none', set=['C004W',2], act=(['C004W',2], ['C004W',3]), out=['AV0W',1], min=20, max=95, inv=False, bias=0xc000, deadband=1): # svc - name, member. 
         ''' One instance per pwm channel. Generate pwm value to be used with it5888pwm. Based on services, no mbi mba regadd knowledge needed.
             in order to modify the pid parameters, use set...  methods in PID class. Parameter bias is for it5888pwm periodics and phase control.
             If more than one act is given (tuple), then the bigger value is used. 
@@ -36,9 +36,9 @@ class AI2pwm(object):
             self.ac.make_svc(self.set[0], send=False) # generates values based on raw, does not send (otherwise about 3 min delay in true value!)
             self.ac.make_svc(self.act[0], send=False) # no svc send to monitoring
             self.setval = self.ac.get_aivalue(self.set[0], self.set[1])[0] # get_aivalue() returns tuple, lo hi included!
-            if 'list' in str(type self.act):  # one act channel
+            if 'list' in str(type(self.act)):  # one act channel
                 self.actval = self.ac.get_aivalue(self.act[0], self.act[1])[0]
-            elif 'tuple' in str(type self.act):  # more than one act, select the one with biggest abs value
+            elif 'tuple' in str(type(self.act)):  # more than one act, select the one with biggest abs value
                 actval = 0; self.actval = 0
                 for i in range(len(self.act)):
                     actval = self.ac.get_aivalue(self.act[0], self.act[i][1])[0]
